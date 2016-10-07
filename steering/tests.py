92a9e9dca7cf9886b5ec8fa7d6c953c5e3aa1236
Ariel Nuñez <ingenieroariel@gmail.com>
2016-10-07 18:56:40 -0500

    Do something with the imports

--- steering/tests.py
+++ steering/tests.py
@@ -3,5 +3,8 @@
 def test_imports():
     """Check we can load tensorflow and keras.
     """
-    import keras
     import tensorflow
+    import keras
+
+    assert tensorflow.__version__ is not None
+    assert keras.__version__ is not None
