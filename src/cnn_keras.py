c79438f8f61d1cfaceaa013eb8af7cfc61b28163
Jorge <jorgemg0327@gmail.com>
2016-10-07 12:07:10 -0500

    Fixed typo

--- src/cnn_keras.py
+++ src/cnn_keras.py
@@ -112,7 +112,7 @@ datagen = ImageDataGenerator(
     featurewise_center=True,  # set input mean to 0 over the dataset
     samplewise_center=True,  # set each sample mean to 0
     featurewise_std_normalization=True,  # divide inputs by std of the dataset
-    samplewise_std_normalization=True,  # divide each input by its std
+    samplewise_std_normalization=True)  # divide each input by its std
 
 datagen.fit(X_train)
 
